3ccee83b4daf147611e79b8f88339a145d246f97
stephenonchieku1 <onchiekustephen901@gmail.com>
2025-02-25 14:38:16 +0300

    :generate clothing item controller

---
+++ app/Http/Controllers/ClothingItemController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClothingItem;
+use Illuminate\Http\Request;
+
+class ClothingItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClothingItem $clothingItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClothingItem $clothingItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ClothingItem $clothingItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClothingItem $clothingItem)
+    {
+        //
+    }
+}
