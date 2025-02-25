ed5e65c6154ddc158413fc22adeea544838a6f14
stephenonchieku1 <onchiekustephen901@gmail.com>
2025-02-25 16:37:09 +0300

    :sparkles: create  data seeder & and add  data for the table

---
+++ database/seeders/ClothingItemSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClothingItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ClothingItem::insert([
+            [
+                'name' => 'Classic T-Shirt',
+                'category' => 'Tops',
+                'brand' => 'Nike',
+                'size' => 'M',
+                'image' => 'tshirt.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Denim Jeans',
+                'category' => 'Bottoms',
+                'brand' => 'Levi’s',
+                'size' => '32',
+                'image' => 'jeans.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Running Shoes',
+                'category' => 'Shoes',
+                'brand' => 'Adidas',
+                'size' => '9',
+                'image' => 'shoes.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
