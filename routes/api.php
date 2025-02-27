3550b59fedd08578f8e91b1ab843324c845fe759
stephenonchieku1 <onchiekustephen901@gmail.com>
2025-02-27 11:25:34 +0300

    Add routes for login&sigup&logout&clothingitems

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClothingItemController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
+
+
+Route::post('/signin', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/clothing-items', [ClothingItemController::class, 'store']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('clothing-items', ClothingItemController::class);
-});
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::apiResource('clothing-items', ClothingItemController::class);
+Route::apiResource('items', CItemController::class);
